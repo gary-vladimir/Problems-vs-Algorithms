66cff3709c79991b6a31b82db1d045e4e5bfa0d8
gary-vladimir <gary@garybricks.com>
2021-12-20 13:50:26 -0600

    problem 1 solved

--- Problem_1.py
+++ Problem_1.py
@@ -1,15 +1,22 @@
+def helper(number):
+    low = 0
+    high = number
+
+    while low <= high:
+        mid = (low + high) // 2
+        if (mid**2 == number) or (mid**2 <= number and (mid+1)**2 > number):
+            return mid
+        elif (mid**2 > number):
+            high = mid
+        else:
+            low = mid
+
+
 def sqrt(number):
-    """
-    Calculate the floored square root of a number
-
-    Args:
-       number(int): Number to find the floored squared root
-    Returns:
-       int: Floored Square Root
-    """
-    pass
+    return -1 if number < 0 else 1 if number == 1 else helper(number)
 
 
+print("Pass" if (-1 == sqrt(-25)) else "Fail")
 print("Pass" if (3 == sqrt(9)) else "Fail")
 print("Pass" if (0 == sqrt(0)) else "Fail")
 print("Pass" if (4 == sqrt(16)) else "Fail")
