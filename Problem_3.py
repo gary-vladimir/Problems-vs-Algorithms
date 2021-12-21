198f086f01375e826781c5318503f5bc934da052
gary-vladimir <gary@garybricks.com>
2021-12-21 13:45:09 -0600

    finished problem 3

--- Problem_3.py
+++ Problem_3.py
@@ -6,6 +6,16 @@ def rearrange_digits(input_list):
     if size <= 2:
         return input_list
 
+    quicksort(input_list)
+
+    first, second = '', ''
+    for i in range(size - 1, -1, -2):
+        first += str(input_list[i])
+        if i - 1 >= 0:
+            second += str(input_list[i - 1])
+
+    return [int(first), int(second)]
+
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -17,4 +27,6 @@ def test_function(test_case):
 
 
 test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
+test_function([[1, 2], [1, 2]])
+test_function([[1, 2, 3], [1, 32]])
